ceaf14d8ddc362fde1b15a8c34ff82c3481460a6
Matias <elnegrochoque@gmail.com>
2021-04-13 09:29:30 -0300

    primer commit

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,7 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
 const AgregarProducto = () => {
+
+  const [nombreProducto, setNombreProducto]=useState('');
+  const [precioProducto, setPrecioProducto]=useState('');
+  const [categoia, setCategoria]= useState('');
+  
+
   return (
     <Fragment>
       <Container className='my-4'>
@@ -9,11 +15,11 @@ const AgregarProducto = () => {
           <h1 className='my-4 text-center'>Agregar nuevo producto</h1>
           <Form.Group>
             <Form.Label>Nombre del producto</Form.Label>
-            <Form.Control type="text" placeholder="Submarino"></Form.Control>
+            <Form.Control type="text" placeholder="Submarino" onChange={(e)=> setNombreProducto(e.target.value)}></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>Precio</Form.Label>
-            <Form.Control type="number" placeholder="$50"></Form.Control>
+            <Form.Control type="number" placeholder="$50" onChange={(e)=> setPrecioProducto(e.target.value)}></Form.Control>
           </Form.Group>
           <div className='text-center my-4'>
           <h3>Categoria</h3>
@@ -22,30 +28,40 @@ const AgregarProducto = () => {
               type="radio"
               label="Bebida caliente"
               name='categoria'
+              value='bebida-caliente'
+              onChange={(e)=> setCategoria(e.target.value)}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Bebida Fria"
               name='categoria'
+              value='bebida-fria'
+              onChange={(e)=> setCategoria(e.target.value)}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Sandwich"
               name='categoria'
+              value='sandwich'
+              onChange={(e)=> setCategoria(e.target.value)}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Dulce"
               name='categoria'
+              value='dulce'
+              onChange={(e)=> setCategoria(e.target.value)}
             ></Form.Check>
             <Form.Check
               inline
               type="radio"
               label="Salado"
               name='categoria'
+              value='salado'
+              onChange={(e)=> setCategoria(e.target.value)}
             ></Form.Check>
           </div>
           <Button variant='danger' type='submit' className='w-100'>Agregar Producto</Button>
